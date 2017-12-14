9f09251a8b114ccdfeec69dac6cf76a99ae27985
aljoh <reachingalfred@gmail.com>
2017-12-14 10:00:56 +0100

    logic added to calculation

---
+++ src/providers/person/person.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+import { CooperProvider } from '../cooper/cooper';
+
+
+@Injectable()
+export class PersonProvider {
+  public gender: string;
+  public age: number;
+  public assessmentMessage: string;
+
+  constructor(private cooper: CooperProvider) {}
+
+  doAssessment(distance: number): void {
+    this.assessmentMessage = this.cooper.assess(this, distance);
+  }
+
+}
